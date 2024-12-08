cab00df442684c3297bc24f36ffe57c7bdcc1ae0
sulincix <aliriza.keskin@pardus.org.tr>
2024-12-08 22:51:57 +0000

    Create C-like-class.py

---
+++ C-like-class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+class amogus:
+    pass
+
+def amogus_print(ctx, arg):
+    print(arg, ctx.text)
+
+def amogus_new():
+    a = amogus()
+    a.text = "Hello World"
+    return a
+
+a = amogus_new()
+amogus_print(a, "Cursed Python")
